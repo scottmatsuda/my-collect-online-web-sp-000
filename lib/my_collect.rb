55b8d8206c979b41ce21348603d1b75e5c66f361
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-27 05:39:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array)
+  my_collect = []
   counter = 0
   while counter < array.length
-    yield(array[counter]) 
+    my_collect << yield(array[counter]) 
     counter += 1
   end
+  my_collect
 end
 
